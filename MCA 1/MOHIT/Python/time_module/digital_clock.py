0637675e0210d9aeac6986e9b5fce6a89b9ed14a
VIRAJ MARU <maruviraj11@gmail.com>
2026-03-14 12:17:31 +0530

    Delete MCA 1/MOHIT directory

--- MCA 1/MOHIT/Python/time_module/digital_clock.py
+++
@@ -1,6 +0,0 @@
-import time
-
-while True:
-    now = time.strftime("%H:%M:%S")   # Hours:Minutes:Seconds
-    print(now, end="\r")              # overwrite the same line
-    time.sleep(1)                     # wait 1 second
--- MCA 1/MOHIT/Python/time_module/keyword_args.py
+++
@@ -1,6 +0,0 @@
-def add(**kwargs):
-    return sum(kwargs.values())
-
-x=19
-y=20
-print(add(x=19,x=20))
